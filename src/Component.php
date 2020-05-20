18d8bf53286c2bdf597d3d1551a4bb4a3cd2b165
Leonardo Losoviz <leo@getpop.org>
2020-05-20 18:12:31 +0800

    Initialize Component manually

---
+++ src/Component.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\PostMeta;
+
+use PoP\Root\Component\AbstractComponent;
+
+/**
+ * Initialize component
+ */
+class Component extends AbstractComponent
+{
+    // const VERSION = '0.1.0';
+
+    public static function getDependedComponentClasses(): array
+    {
+        return [
+            \PoP\Meta\Component::class,
+            \PoP\Posts\Component::class,
+        ];
+    }
+
+    public static function getDependedMigrationPlugins(): array
+    {
+        return [
+            'migrate-postmeta',
+        ];
+    }
+}
