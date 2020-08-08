88eb15f3dbf65e023ebc1d3fa5de1b22e956b89f
Leonardo Losoviz <leo@getpop.org>
2020-08-08 16:49:37 +0800

    Include owner in depended migration plugin

--- src/Component.php
+++ src/Component.php
@@ -24,7 +24,7 @@ class Component extends AbstractComponent
     public static function getDependedMigrationPlugins(): array
     {
         return [
-            'migrate-custompostmeta',
+            'pop-schema/migrate-custompostmeta',
         ];
     }
 }
